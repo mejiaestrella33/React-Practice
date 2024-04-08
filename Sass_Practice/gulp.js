1a5f7e736856bfc6fb5dffc157ff80ffbd9cd3a7
estrella-mejia <estrella.mejia@innova.com>
2024-04-07 19:05:10 -0700

    updating Plush Shop for react course and Sass practice to work on remotely

--- Sass_Practice/gulp.js
+++ Sass_Practice/gulp.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("ccss"));
+  return src("found/**/*.scss").pipe(sass()).pipe(dest("ccss"));
 }
 
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(["found/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStylesm, watchTask);
